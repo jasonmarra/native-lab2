290e9395e8ff9256de346a63b825b5010160d5d3
jason marra <jmarra_08@icloud.com>
2025-09-05 09:17:05 -0400

    update

--- TicketMachine.java
+++ TicketMachine.java
@@ -17,6 +17,8 @@ public class TicketMachine
     private int balance;
     // The total amount of money collected by this machine.
     private int total;
+    
+    private int status;
 
     /**
      * Create a machine that issues tickets of the given price.
@@ -25,11 +27,23 @@ public class TicketMachine
      */
     public TicketMachine(int cost)
     {
-        price = cost;
+        price =1000;
+        price= cost;
         balance = 0;
         total = 0;
     }
-
+    public void empty()
+    {
+        total=0;
+    }
+    public int getTotal()
+    {
+        return total;
+    }
+    public void prompt()
+    {
+        System.out.println("Please insert the correct amount of money.");
+    }
     /**
      * Return the price of a ticket.
      */
@@ -42,7 +56,7 @@ public class TicketMachine
      * Return the amount of money already inserted for the
      * next ticket.
      */
-    public int getBalance()
+    public int getAmount()
     {
         return balance;
     }
@@ -66,7 +80,7 @@ public class TicketMachine
         System.out.println("##################");
         System.out.println("# The BlueJ Line");
         System.out.println("# Ticket");
-        System.out.println("# " + price + " cents.");
+        System.out.printf("# %d cents. %n", price);
         System.out.println("##################");
         System.out.println();
 
